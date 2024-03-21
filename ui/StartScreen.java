d84a873d4d184227288e511ab40e4b9bea905133
junnie082 <junnie082@gmail.com>
2024-03-21 12:20:30 +0900

    feat: added Setting, Score Board, Exit buttons

--- ui/StartScreen.java
+++ ui/StartScreen.java
@@ -7,21 +7,34 @@ public class StartScreen extends JFrame implements ActionListener {
 
     public StartScreen() {
         setTitle("Tetris");
-        setSize(800, 500);
-        setLocation(100, 100);
+        setSize(200, 300); // Adjusted size for demonstration
+        setLocationRelativeTo(null); // Centered window
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
 
-        JButton btnGameStart = new JButton("Game Start");
-        btnGameStart.setActionCommand("gameStart");
-        btnGameStart.addActionListener(this);
+        // Use BoxLayout with Y_AXIS alignment for vertical alignment
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JButton btnGameStart = createBtn("Game Start", "gameStart");
+        JButton btnOption = createBtn("Setting", "setting");
+        JButton btnScoreBoard = createBtn("Score Board", "scoreboard");
+        JButton btnExit = createBtn("Exit", "exit");
+
+        // Add buttons to the frame
         add(btnGameStart);
-
-        JPanel panel = new JPanel();
-        panel.add(btnGameStart);
-        add(panel);
+        add(btnOption);
+        add(btnScoreBoard);
+        add(btnExit);
 
         setVisible(true);
+    }
+
+    public JButton createBtn(String btnName, String command) {
+        JButton button = new JButton(btnName);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT); // Center align button
+        button.setActionCommand(command);
+        button.addActionListener(this);
+
+        return button;
     }
 
     @Override
@@ -29,7 +42,12 @@ public class StartScreen extends JFrame implements ActionListener {
         String command = e.getActionCommand();
         if (command.equals("gameStart")) {
             System.out.println("Game Start!!!");
+        } else if (command.equals("setting")) {
+            System.out.println("Setting!!!");
+        } else if (command.equals("scoreboard")) {
+            System.out.println("Score Board!!!");
+        } else if (command.equals("exit")) {
+            System.out.println("Exit!!!");
         }
     }
-
 }
