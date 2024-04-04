fdf8248cea0bc5d91b468be5ca3ff42b81c47d13
woogie01 <kwonhu3973@gmail.com>
2024-04-04 11:20:15 +0900

    Fix, 설정 화면 및 스코어보드 화면에서 메뉴 버튼에 대한 마우스 반응이 나타나지 않는 버그 수정

--- src/component/Button.java
+++ src/component/Button.java
@@ -57,6 +57,27 @@ public class Button {
         return button;
     }
 
+    static public JButton createLogoBtn(String command, ActionListener listener, String iconPath) {
+        JButton button = new JButton();
+        button.setAlignmentX(Component.CENTER_ALIGNMENT); // Center align button
+
+        // 이미지 경로가 제공되었으면 이미지 아이콘을 버튼에 추가
+        if (iconPath != null && !iconPath.isEmpty()) {
+            ImageIcon icon = new ImageIcon(iconPath);
+
+            // 이미지 크기 조정
+            Image image = icon.getImage();
+            // 예시로, 24x24 픽셀 크기로 이미지 조정
+            Image newImg = image.getScaledInstance(25, 25,  java.awt.Image.SCALE_SMOOTH);
+            icon = new ImageIcon(newImg);
+            button.setIcon(icon);
+        }
+
+        button.setActionCommand(command);
+        button.addActionListener(listener);
+        return button;
+    }
+
     // 아이콘 경로 없이 버튼을 생성하는 메서드
     static public JButton createBtn(String btnName, String command, ActionListener listener) {
         JButton button = new JButton(btnName);
