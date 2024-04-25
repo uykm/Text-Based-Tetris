7be594d52f7adb33008b992b313810dc091a683b
Minkyu Shin <digi1k2001@gmail.com>
2024-04-26 08:23:19 +0900

    Main 수정

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-=======
 import ui.MainMenuScreen;
 
-
->>>>>>> develop
 public class Main {
 
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-<<<<<<< HEAD
 }
-=======
-}
-
->>>>>>> develop
