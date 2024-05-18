eb856cb8138782f988c7104093fa92f349443d5c
junnie082 <junnie082@gmail.com>
2024-05-18 14:23:31 +0900

    feat: 패키징 및 아이콘 설정

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,9 @@
-module $MODULE_NAME$ {
+module com.tetris.java {
+
+    requires java.desktop;
+
+    exports com.tetris.component;
+    exports com.tetris.logic;
+    exports com.tetris.model;
+    exports com.tetris.ui;
 }
