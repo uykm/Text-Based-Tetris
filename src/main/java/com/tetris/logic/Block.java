0af1edc8fd94e8e10285eec4e7dd57f9db6662ea
Minkyu Shin <digi1k2001@gmail.com>
2024-05-20 16:30:02 +0900

    디버깅 코드 제거

--- src/main/java/com/tetris/logic/Block.java
+++ src/main/java/com/tetris/logic/Block.java
@@ -16,7 +16,7 @@ public abstract class Block {
     public boolean canMoveSide;
 
     // 아이템 블럭이 생성될 수 있는 지워진 줄의 개수
-    final private int erasedLineCountToCreateItemBlock = 2;
+    final private int erasedLineCountToCreateItemBlock = 10;
 
     // stopCount: 조작이 없으면 1씩 증가,
     private int stopCount = 0;
@@ -179,7 +179,7 @@ public abstract class Block {
             // 일반 블록 선택 로직 (10줄이 삭제되지 않았을 때)
             int blockType = rwSelection.select();
             // 기본 블록 선택 로직
-            return switch (0) {
+            return switch (blockType) {
                 case 0 -> new IBlock();
                 case 1 -> new JBlock();
                 case 2 -> new LBlock();
